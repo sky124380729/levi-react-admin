3e48b19c7de693130127616873b91a7290e28828
pink <jerry.zheng@cloudkeeper.cn>
2020-09-04 09:58:10 +0800

    layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from 'react'
 import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons'
-import { Layout as Wrapper, Menu } from 'antd'
+import { Layout as Wrapper, Menu, Breadcrumb } from 'antd'
 import logo from './logo.png'
 import './style.scss'
 
@@ -8,7 +8,6 @@ const { Header, Sider, Content } = Wrapper
 
 const Layout: FC = () => {
     const [collapsed, setCollapsed] = useState(false)
-
     return (
         <Wrapper className='levi-layout'>
             <Sider trigger={null} collapsible collapsed={collapsed}>
@@ -20,7 +19,18 @@ const Layout: FC = () => {
                     <Menu.SubMenu key='2' title='nav 2'>
                         <Menu.Item key='2-1'>nav 2-1</Menu.Item>
                         <Menu.Item key='2-2'>nav 2-2</Menu.Item>
+                        <Menu.Item key='2-3'>nav 2-3</Menu.Item>
+                        <Menu.Item key='2-4'>nav 2-4</Menu.Item>
+                        <Menu.Item key='2-5'>nav 2-5</Menu.Item>
+                        <Menu.Item key='2-6'>nav 2-6</Menu.Item>
+                        <Menu.Item key='2-7'>nav 2-7</Menu.Item>
+                        <Menu.Item key='2-8'>nav 2-8</Menu.Item>
+                        <Menu.Item key='2-9'>nav 2-9</Menu.Item>
+                        <Menu.Item key='2-10'>nav 2-10</Menu.Item>
+                        <Menu.Item key='2-11'>nav 2-11</Menu.Item>
+                        <Menu.Item key='2-12'>nav 2-12</Menu.Item>
                     </Menu.SubMenu>
+                    <Menu.Item key='3'>nav 3</Menu.Item>
                 </Menu>
             </Sider>
             <Wrapper className='site-layout'>
@@ -31,6 +41,10 @@ const Layout: FC = () => {
                             setCollapsed(!collapsed)
                         }
                     })}
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
                 </Header>
                 <Content
                     className='site-layout-background'
